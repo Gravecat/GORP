5e7755e87985bee144b2de1647a3a8b3b08ca77d
Raine 'Gravecat' Simmons <gc@gravecat.com>
2025-08-30 18:14:15 +0100

    Procedural island generation can now determine contiguous land-masses via flood-fill.

--- src/procgen/island.cpp
+++ src/procgen/island.cpp
@@ -13,25 +13,117 @@
 #include "util/math/mathutils.hpp"
 #include "util/math/random.hpp"
 
+#include "core/core.hpp"
+
 namespace gorp {
 
 // Generates a new island of the specified size.
-IslandProcGen::IslandProcGen(uint16_t size, uint32_t seed) : canvas_id_(0), canvas_ptr_(nullptr), seed_(seed), size_(size)
+IslandProcGen::IslandProcGen(uint16_t size, uint32_t seed) : seed_(seed), size_(size)
 {
     if (!seed) seed_ = random::get<uint32_t>(INT_MAX);
     if (size < ISLAND_SIZE_MIN || size > ISLAND_SIZE_MAX) throw GuruMeditation("Invalid island size!", size, ISLAND_SIZE_MAX);
 
-    // This should only be used for testing and development, never for normal gameplay!
-    if (GENERATE_DEV_MAP)
+    generate_heightmap();
+    determine_sub_islands();
+}
+
+// Determines which land-masses are contiguous, and defines these as sub-islands.
+void IslandProcGen::determine_sub_islands()
+{
+    // If we're generating dev maps, we need to create a new canvas here.
+    DevCanvas* canvas_ptr = nullptr;
+    if (GENERATE_DEV_MAPS)
     {
-        canvas_id_ = game().add_element(std::make_unique<DevCanvas>(Vector2u(size_, size_)));
-        DevCanvas& canvas_ref = static_cast<DevCanvas&>(game().element(canvas_id_));
-        canvas_ptr_ = &canvas_ref;
+        uint32_t canvas_id = game().add_element(std::make_unique<DevCanvas>(Vector2u(size_, size_)));
+        DevCanvas& canvas_ref = static_cast<DevCanvas&>(game().element(canvas_id));
+        canvas_ptr = &canvas_ref;
+    }
+    
+    sub_island_id_.resize(size_ * size_, SUB_ISLAND_ID_UNDEFINED);
+    unsigned int current_sub_id = 0;
+    for (unsigned int x = 0; x < size_; x++)
+    {
+        for (unsigned int y = 0; y < size_; y++)
+        {
+            const uint32_t index = mathutils::array_index({x, y}, {size_, size_});
+            const float height = height_map_.at(index);
+            if (height <= HEIGHT_MAP_WATER)
+            {
+                sub_island_id_.at(index) = SUB_ISLAND_ID_WATER;
+                continue;
+            }
+            else if (sub_island_id_.at(index) == SUB_ISLAND_ID_UNDEFINED) floodfill_sub_islands({x, y}, current_sub_id++, canvas_ptr);
+        }
     }
 }
 
-// Destructor, cleans up after ourselves.
-IslandProcGen::~IslandProcGen() { if (canvas_id_) game().delete_element(canvas_id_); }
+// Flood-fills an area starting at the coordinates, with the specified ID.
+void IslandProcGen::floodfill_sub_islands(Vector2u start, unsigned int id, DevCanvas* canvas_ptr)
+{
+    const uint32_t index = mathutils::array_index({start.x, start.y}, {size_, size_});
+    if (sub_island_id_.at(index) != SUB_ISLAND_ID_UNDEFINED) return;    // Do nothing if this tile has already been flood-filled.
+    if (height_map_.at(index) <= HEIGHT_MAP_WATER) return;  // Do nothing if this tile is water level or below.
+
+    sub_island_id_.at(index) = id;  // Set the sub-island ID for this tile.
+
+    // If an existing sub_island_coords_ vector does not yet exist for this ID, make a new one.
+    if (sub_island_coords_.size() < id + 1)
+    {
+        std::vector<Vector2u> new_vec;
+        new_vec.push_back(start);
+        sub_island_coords_.push_back(new_vec);
+    }
+    // Otherwise, just insert our current coordinates into the existing vector.
+    else sub_island_coords_.at(id).push_back(start);
+
+    // If we're generating dev maps, now's a good time to draw the islands.
+    if (GENERATE_DEV_MAPS && canvas_ptr)
+    {
+        Colour colour = Colour::GRAY_DARK;
+        switch(id)
+        {
+            case 0: colour = Colour::RED; break;
+            case 1: colour = Colour::ORANGE; break;
+            case 2: colour = Colour::YELLOW; break;
+            case 3: colour = Colour::GREEN; break;
+            case 4: colour = Colour::CYAN; break;
+            case 5: colour = Colour::BLUE; break;
+            case 6: colour = Colour::PURPLE; break;
+            case 7: colour = Colour::BROWN; break;
+            case 8: colour = Colour::RED_LIGHT; break;
+            case 9: colour = Colour::ORANGE_LIGHT; break;
+            case 10: colour = Colour::YELLOW_LIGHT; break;
+            case 11: colour = Colour::GREEN_LIGHT; break;
+            case 12: colour = Colour::CYAN_LIGHT; break;
+            case 13: colour = Colour::BLUE_LIGHT; break;
+            case 14: colour = Colour::PURPLE_LIGHT; break;
+            case 15: colour = Colour::BROWN_LIGHT; break;
+            case 16: colour = Colour::RED_DARK; break;
+            case 17: colour = Colour::ORANGE_DARK; break;
+            case 18: colour = Colour::YELLOW_DARK; break;
+            case 19: colour = Colour::GREEN_DARK; break;
+            case 20: colour = Colour::CYAN_DARK; break;
+            case 21: colour = Colour::BLUE_DARK; break;
+            case 22: colour = Colour::PURPLE_DARK; break;
+            case 23: colour = Colour::BROWN_DARK; break;
+            case 24: colour = Colour::GRAY; break;
+            case 25: colour = Colour::WHITE; break;
+        }
+        canvas_ptr->put(Glyph::FULL_BLOCK, Vector2(start.x, start.y), colour);
+    }
+
+    // Check neighbouring tiles.
+    for (int x = -1; x <= 1; x++)
+    {
+        for (int y = -1; y <= 1; y++)
+        {
+            if (x == 0 && y == 0) continue;
+            if (static_cast<int>(start.x) + x < 0 || start.x + x >= size_) continue;
+            if (static_cast<int>(start.y) + y < 0 || start.y + y >= size_) continue;
+            floodfill_sub_islands(Vector2u(start.x + x, start.y + y), id, canvas_ptr);
+        }
+    }
+}
 
 // Generates the heightmap of the island, based on Perlin noise followed by some other tweaks.
 void IslandProcGen::generate_heightmap()
@@ -43,7 +135,7 @@ void IslandProcGen::generate_heightmap()
     {
         for (unsigned int y = 0; y < size_; y++)
         {
-            const float noise = perlin.octave2D_01((x * 0.10), (y * 0.10), 4);
+            const float noise = perlin.octave2D_01((x * PERLIN_ZOOM), (y * PERLIN_ZOOM), 4);
             height_map_.at(mathutils::array_index({x, y}, {size_, size_})) = noise;
         }
     }
@@ -55,7 +147,7 @@ void IslandProcGen::generate_heightmap()
         const float dx = x - centre, dy = y - centre;
         const float distance = std::sqrt((dx * dx) + (dy * dy));
         const float max_distance = std::sqrt(2) * centre;
-        return (distance / max_distance) * 0.6f;
+        return (distance / max_distance) * ISLAND_HEIGHT_MODIFIER;
     };
     for (unsigned int x = 0; x < size_; x++)
     {
@@ -67,9 +159,11 @@ void IslandProcGen::generate_heightmap()
             height_map_.at(index) -= get_height_modifier(x, y);
 
             // We're also gonna ensure the map border is oceans, and the next couple of tiles in are similarly lowered.
-            if (!x || !y || x == size_ - 1u || y == size_ - 1u) height_map_.at(index) = 0.0f;
-            else if (x == 1 || y == 1 || x == size_ - 2u || y == size_ - 2u) height_map_.at(index) = std::min(height_map_.at(index) - 0.2f, 0.2f);
-            else if (x == 2 || y == 2 || x == size_ - 3u || y == size_ - 3u) height_map_.at(index) = std::min(height_map_.at(index) - 0.1f, 0.3f);
+            if (!x || !y || x == size_ - 1u || y == size_ - 1u) height_map_.at(index) = 0.0f;   // The outer border is always minimum-height.
+            else if (x == 1 || y == 1 || x == size_ - 2u || y == size_ - 2u) height_map_.at(index) = std::min(height_map_.at(index) - BORDER_MODIFIER_OUTER,
+                HEIGHT_MAP_WATER);
+            else if (x == 2 || y == 2 || x == size_ - 3u || y == size_ - 3u) height_map_.at(index) = std::min(height_map_.at(index) - BORDER_MODIFIER_INNER,
+                HEIGHT_MAP_LOWLAND);
         }
     }
 
@@ -94,27 +188,29 @@ void IslandProcGen::generate_heightmap()
             }
 
             if (current_level <= highest_neighbour) continue;
-            else if (highest_neighbour <= 0.1f) height_map_.at(index) = 0.0f;
-            else if (highest_neighbour <= 0.2f) height_map_.at(index) = 0.2f;
+            else if (highest_neighbour <= HEIGHT_MAP_DEEP_WATER) height_map_.at(index) = HEIGHT_MAP_DEEP_WATER;
+            else if (highest_neighbour <= HEIGHT_MAP_WATER) height_map_.at(index) = HEIGHT_MAP_WATER;
         }
     }
 
     // If we're generating dev maps, at this point we'll draw the final result.
-    if (GENERATE_DEV_MAP == 1)
+    if (GENERATE_DEV_MAPS)
     {
+        uint32_t canvas_id = game().add_element(std::make_unique<DevCanvas>(Vector2u(size_, size_)));
+        DevCanvas& canvas_ref = static_cast<DevCanvas&>(game().element(canvas_id));
         for (unsigned int x = 0; x < size_; x++)
         {
             for (unsigned int y = 0; y < size_; y++)
             {
                 const float noise = height_map_.at(mathutils::array_index({x, y}, {size_, size_}));
                 Colour col = Colour::GREEN;
-                if (noise <= 0.1f) col = Colour::BLUE_DARK;
-                else if (noise <= 0.2f) col = Colour::BLUE;
-                else if (noise <= 0.3f) col = Colour::GREEN_LIGHT;
-                else if (noise >= 0.8f) col = Colour::WHITE;
-                else if (noise >= 0.7f) col = Colour::GRAY;
-                else if (noise >= 0.6f) col = Colour::GREEN_DARK;
-                canvas_ptr_->put(Glyph::FULL_BLOCK, Vector2(x, y), col);
+                if (noise <= HEIGHT_MAP_DEEP_WATER) col = Colour::BLUE_DARK;
+                else if (noise <= HEIGHT_MAP_WATER) col = Colour::BLUE;
+                else if (noise <= HEIGHT_MAP_LOWLAND) col = Colour::GREEN_LIGHT;
+                else if (noise >= HEIGHT_MAP_MOUNTAIN_PEAK) col = Colour::WHITE;
+                else if (noise >= HEIGHT_MAP_MOUNTAIN) col = Colour::GRAY;
+                else if (noise >= HEIGHT_MAP_HIGHLAND) col = Colour::GREEN_DARK;
+                canvas_ref.put(Glyph::FULL_BLOCK, Vector2(x, y), col);
             }
         }
     }
--- src/procgen/island.hpp
+++ src/procgen/island.hpp
@@ -16,20 +16,39 @@ class IslandProcGen {
 public:
     IslandProcGen() = delete;   // No default constructor.
     IslandProcGen(uint16_t size, unsigned int seed = 0);    // Generates a new island of the specified size, with an optional PRNG seed.
-    ~IslandProcGen();   // Destructor, cleans up after ourselves.
 
 private:
-    void generate_heightmap();  // Generates the heightmap of the island, based on Perlin noise followed by some other tweaks.
+    void    determine_sub_islands();        // Determines which land-masses are contiguous, and defines these as sub-islands.
+    void    floodfill_sub_islands(Vector2u start, unsigned int id, DevCanvas* canvas_ptr);  // Flood-fills an area starting at the coords with the specified ID.
+    void    generate_heightmap();           // Generates the heightmap of the island, based on Perlin noise followed by some other tweaks.
 
-    static constexpr int        GENERATE_DEV_MAP = 1;       // Used for visual feedback during development/debugging/testing.
-    static constexpr uint16_t   ISLAND_SIZE_MAX =   512;    // The largest allowed island size.
-    static constexpr uint16_t   ISLAND_SIZE_MIN =   16;     // The smallest sllowed island size.
+    static constexpr bool       GENERATE_DEV_MAPS =         true;   // Used for visual feedback during development/debugging/testing.
 
-    uint32_t    canvas_id_;     // The ID of the DevCanvas Element we'll be drawing debug maps to.
-    DevCanvas*  canvas_ptr_;    // The DevCanvas used for displaying debugging/development information.
+    static constexpr uint16_t   ISLAND_SIZE_MAX =           512;    // The largest allowed island size.
+    static constexpr uint16_t   ISLAND_SIZE_MIN =           16;     // The smallest sllowed island size.
+
+    static constexpr float      BORDER_MODIFIER_INNER =     0.1f;   // The fixed reduction in height for the inner border of the map.
+    static constexpr float      BORDER_MODIFIER_OUTER =     0.2f;   // As above, but for the outer border.
+    static constexpr float      ISLAND_HEIGHT_MODIFIER =    0.6f;   // The distance-from-centre modifier that adjusts the island height, providing a coatline.
+
+    static constexpr float      PERLIN_OCTAVES =            4;      // The number of Perlin noise octaves.
+    static constexpr float      PERLIN_ZOOM =               0.1f;   // The Perlin noise zoom level.
+
+    static constexpr float      HEIGHT_MAP_DEEP_WATER =     0.1f;   // Any tile heights at this point or below are deep water.
+    static constexpr float      HEIGHT_MAP_WATER =          0.2f;   // As above, but for regular water.
+    static constexpr float      HEIGHT_MAP_LOWLAND =        0.3f;   // The lowest level of terrain above water.
+    static constexpr float      HEIGHT_MAP_HIGHLAND =       0.6f;   // At this point or above, we get into the highlands.
+    static constexpr float      HEIGHT_MAP_MOUNTAIN =       0.7f;   // Mountains begin at this level.
+    static constexpr float      HEIGHT_MAP_MOUNTAIN_PEAK =  0.8f;   // Mountain peaks at the highest levels.
+
+    static constexpr int        SUB_ISLAND_ID_UNDEFINED =   -1;     // Sub-island ID has not yet been set.
+    static constexpr int        SUB_ISLAND_ID_WATER =       -2;     // This tile is water, and cannot belong to a sub-island.
+
     std::vector<float>  height_map_;    // The height map of the island, which determines the terrain.
     uint32_t    seed_;          // The PRNG seed, used to (hopefully) generate identical islands with the same seed.
     uint16_t    size_;          // The size of this island map. Limited to uint16_t because any larger would just be ridiculous.
+    std::vector<std::vector<Vector2u>>  sub_island_coords_; // Coordinates for each sub-island on the generated map.
+    std::vector<int>    sub_island_id_; // Sub-island ID markers for each coordinate on the map.
 };
 
 }   // namespace gorp
